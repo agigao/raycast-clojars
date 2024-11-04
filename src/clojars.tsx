4be125e0f50a871b78c85fb3a8a3002a611045d3
Giga Chokheli <giga@chokhe.li>
2024-11-05 00:21:09 +0400

    Working prototype

--- src/clojars.tsx
+++ src/clojars.tsx
@@ -1,17 +1,55 @@
-import { ActionPanel, Detail, List, Action, Icon } from "@raycast/api";
+import { ActionPanel, Action, List, showToast, Toast } from "@raycast/api";
+import { useState } from "react";
+import { useFetch } from "@raycast/utils";
+
+type Package = {
+  jar_name: string;
+  group_name: string;
+  version: string;
+  description: string;
+  created: string;
+};
+
+const CLOJARS_API_URL = "https://clojars.org/search";
 
 export default function Command() {
+  const [query, setQuery] = useState("");
+
+  // Perform the API request with useFetch, only if there's a query
+  const { isLoading, data, error } = useFetch<{ results: Package[] }>(
+    `${CLOJARS_API_URL}?q=${encodeURIComponent(query)}&format=json`,
+    {
+      execute: !!query,
+    },
+  );
+
+  if (error) {
+    showToast(Toast.Style.Failure, "Live search failed");
+  }
+
   return (
-    <List>
-      <List.Item
-        icon={Icon.Bird}
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
+    <List
+      searchBarPlaceholder="Search Clojure packages on Clojars..."
+      onSearchTextChange={setQuery}
+      isLoading={isLoading}
+    >
+      {(data?.results || []).map((pkg) => {
+        const formattedText = `${pkg.group_name}/${pkg.jar_name} {:mvn/version "${pkg.version}"}`;
+
+        return (
+          <List.Item
+            key={`${pkg.group_name}/${pkg.jar_name}`} // Combine fields for a unique key
+            title={`${pkg.group_name}/${pkg.jar_name}`}
+            subtitle={pkg.description}
+            accessories={[{ text: `v${pkg.version}` }]}
+            actions={
+              <ActionPanel>
+                <Action.CopyToClipboard title="Copy Dependency" content={formattedText} />
+              </ActionPanel>
+            }
+          />
+        );
+      })}
     </List>
   );
 }
